9a8367d86ebf3948a43b36b03b988ab727237069
Stuart Olsen <stuart@sj-olsen.com>
2023-09-12 18:09:50 -0500

    Trim and compress whitespace

--- render.py
+++ render.py
@@ -1,42 +1,95 @@
 from collections.abc import Iterator
+import dataclasses
 import html.parser
 import textwrap
 
 
+@dataclasses.dataclass
+class Line:
+    _chunks: list[str] = dataclasses.field(default_factory=list)
+
+    def append(self, chunk: str):
+        self._chunks.append(chunk)
+
+    def contents(self) -> str:
+        return ''.join(self._chunks)
+
+
+@dataclasses.dataclass
+class Paragraph:
+    _lines: list[Line] = dataclasses.field(default_factory=list)
+
+    def _last_line(self) -> Line:
+        if not self._lines:
+            self._lines.append(Line())
+        return self._lines[-1]
+
+    def append(self, chunk: str):
+        self._last_line().append(chunk)
+
+    def line_break(self):
+        self._lines.append(Line())
+
+    def lines(self) -> Iterator[str]:
+        for line in self._lines:
+            yield line.contents()
+
+
 class BasicHTML(html.parser.HTMLParser):
-    _lines: list[list[str]]
+    _paragraphs: list[Paragraph]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._lines = []
+        self._paragraphs = []
 
-    def _get_line(self, i: int) -> str:
-        result = ''.join(self._lines[i])
-        self._lines[i] = [result]
-        return result
-
-    def lines(self) -> Iterator[str]:
-        for i in range(len(self._lines)):
-            yield self._get_line(i)
+    def _last_paragraph(self) -> Paragraph:
+        if not self._paragraphs:
+            self._paragraphs.append(Paragraph())
+        return self._paragraphs[-1]
 
     def handle_starttag(self, tag: str, attrs: list[tuple[str, str]]):
-        if tag == 'p' and self._lines:
-            self._lines.append([])
-            self._lines.append([])
+        if tag == 'p':
+            self._paragraphs.append(Paragraph())
         if tag == 'br':
-            self._lines.append([])
+            self._last_paragraph().line_break()
 
     def handle_data(self, data: str):
-        if not self._lines:
-            self._lines.append([])
-        self._lines[-1].append(data)
+        self._last_paragraph().append(data)
+
+    def _raw_lines(self) -> Iterator[str]:
+        for paragraph in self._paragraphs:
+            yield ''
+            yield from paragraph.lines()
+
+    def _compressed_lines(self) -> Iterator[str]:
+        i = self._raw_lines()
+        # Discard leading whitespace
+        while True:
+            match next(i, None):
+                case None:
+                    return
+                case '':
+                    continue
+                case line:
+                    yield line
+                    break
+        # Compress internal whitespace and discard trailing whitespace
+        blank_lines = 0
+        while True:
+            match next(i, None):
+                case None:
+                    return
+                case '':
+                    blank_lines += 1
+                case line:
+                    if blank_lines > 0:
+                        yield ''
+                        blank_lines = 0
+                    yield line
 
     @classmethod
     def render(cls, src: str, *, display_width: int) -> Iterator[str]:
         r = cls()
         r.feed(src)
-        for raw_line in r.lines():
-            output_lines = textwrap.wrap(raw_line, width=display_width)
-            if not output_lines:
-                yield ''
-            yield from output_lines
+        for line in r._compressed_lines():
+            yield from textwrap.wrap(line, width=display_width) or ['']
